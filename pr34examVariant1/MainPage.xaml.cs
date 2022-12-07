c84811b103b1bfeacc157067d23915501986b7e8
305-6 <305-6@305-6>
2022-12-07 13:42:43 +0500

    Добавьте файлы проекта.

---
+++ pr34examVariant1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace pr34examVariant1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (login.Text != "" && password.Text != "")
+            {
+                await Navigation.PushAsync(new Page1());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Введите логин и пароль", "OK");
+            }
+        }
+    }
+}
---
+++ pr34examVariant1/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pr34examVariant1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            int m = Convert.ToInt32(Metri.Text);
+            int m2 = Convert.ToInt32(Metri2.Text);
+            Otvet.Text = (m * m2 * 1.4).ToString();
+            await DisplayAlert("Ответ", $"Результат равен = {Otvet.Text} тысяч рублей ", "ОК");
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
